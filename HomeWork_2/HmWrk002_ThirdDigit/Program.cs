5c63945387f8f882af124c4248ea9be8adc6a341
vladimir_skrypnikov <sevgrad@list.ru>
2022-12-20 18:38:59 +0300

    Comments added to second homework

--- HomeWork_2/HmWrk002_ThirdDigit/Program.cs
+++ HomeWork_2/HmWrk002_ThirdDigit/Program.cs
@@ -5,26 +5,26 @@ Console.Write("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int numberABS = Math.Abs(number);
 
-if (numberABS < 100)
+if (numberABS < 100)                // Если число двузначное или меньше
 {
     Console.WriteLine("Во введенном числе нет третьей цифры.");
 }
-else if (numberABS < 1000)
+else if (numberABS < 1000)          // Если число трехзначное, но не двузначное
 {
     int thirdDigit = numberABS % 10;
     Console.WriteLine($"Третья цифра числа: {thirdDigit}");
 }
-else
+else                                // Если число содержит больше трех разрядов
 {
-    int reduction = numberABS;
-    bool numberDigitFlag = true;
-    int thirdDigit = 0;   
+    int reduction = numberABS;      // Вводим вспомогательную переменную для сокращения разрядности числа
+    bool numberDigitFlag = true;    // Вводим переменную-флаг для завершения цикла
+    int thirdDigit = 0;             // Объявляем переменную для последующего вывода
 
     while (numberDigitFlag)
     {
-        reduction = reduction / 10;        
+        reduction = reduction / 10; // Сокращаем разрядность на один      
 
-        if (reduction < 1000) 
+        if (reduction < 1000)       // Проверяем, не сократилось ли число разрядов до трех
         {
             numberDigitFlag = false;
             thirdDigit = reduction % 10;            
