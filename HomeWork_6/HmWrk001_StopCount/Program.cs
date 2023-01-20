3847de34951f14e0ec1e22cabfe2d637e25da4c5
vlads_geekbrains <sevgrad@list.ru>
2023-01-20 13:29:06 +0300

    Sixth Homework

---
+++ HomeWork_6/HmWrk001_StopCount/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь. Ввод чисел останавливается при помощи ввода стоп-слова "stop" и производится 
+// при помощи нажатия Enter.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+string UsersInput(int index)
+{
+    Console.Write($"Введите {index}-е число: ");
+    string input = Console.ReadLine();
+    return input;    
+}
+
+Console.WriteLine("Вводите целые числа. Чтобы закончить вводить числа наберите 'stop'.");
+int countPositiveNumbers = 0;
+int index = 1;
+bool run = true;
+
+// Будем крутить цикл до тех пор, пока не будет введено "stop"
+while (run)
+{
+    string input = UsersInput(index);
+    bool isNumber = int.TryParse(input, out int inputNumber);
+    if (isNumber && inputNumber > 0) 
+    {
+        countPositiveNumbers++;
+        index++;
+    }
+    else if (isNumber && inputNumber <= 0)
+    {
+        index++;
+    }
+    else if (isNumber == false && input == "stop")
+    {
+        Console.WriteLine(countPositiveNumbers);
+        break;
+    }
+    else Console.WriteLine("Введено некоректное значение.");
+}
---
+++ HomeWork_6/HmWrk002_IntersectionPoint/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void IntersectionPoint(double[] result, double k1, double k2, double b1, double b2)
+{    
+    result[0] = (b2 - b1) / (k1 - k2);
+    result[1] = result[0] * k1 + b1;       
+}
+
+Console.Write("Введите число K1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число K2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2 && b1 != b2) Console.WriteLine("Прямые паралельны.");
+else if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают.");
+else
+{
+    double[] xyResult = new double[2];
+    IntersectionPoint(xyResult, k1, k2, b1, b2);
+    Console.WriteLine($"X: {xyResult[0]}");
+    Console.WriteLine($"Y: {xyResult[1]}");
+}
+
