fa35fcf5530a21cc1b2645a6154b43c3a15aca29
vladimir_skrypnikov <sevgrad@list.ru>
2023-02-12 21:53:16 +0300

    Control Work

---
+++ ControlWork/Program.cs
@@ -0,0 +1,48 @@
+﻿int GetThreeSymbolCount(string[] textArray)
+{
+    int count = 0;
+    for (int i = 0; i < textArray.Length; i++)
+    {
+        if (textArray[i].Length <= 3) count ++;
+    }
+
+    return count;
+}
+
+string[] GetThreeSymbolArray(string[] textArray, int count)
+{    
+    string[] threeSymbolArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < textArray.Length; i++)
+    {
+        if (textArray[i].Length <= 3) threeSymbolArray[j++] = textArray[i];
+    }
+
+    return threeSymbolArray;
+}
+
+void PrintArray(string[] textArray)
+{
+    for (int i = 0; i < textArray.Length; i++)
+    {
+        if (i != textArray.Length - 1)
+        {
+            Console.Write($"{textArray[i]}, ");
+        }
+        else Console.WriteLine($"{textArray[i]}");        
+    }
+}
+
+string[] startArray = {":-)", "Hello", "world", "!", "Bob", "final"};
+
+Console.WriteLine();
+Console.WriteLine("This is start array:");
+PrintArray(startArray);
+
+int countSymbol = GetThreeSymbolCount(startArray);
+string[] finalArray = GetThreeSymbolArray(startArray, countSymbol);
+
+Console.WriteLine("============================");
+Console.WriteLine("This is final array:");
+PrintArray(finalArray);
+
